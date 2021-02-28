92f393eee7ac14cc80148c8b1d38aa907d976be0
Kongjihun <wlgns3914@naver.com>
2021-02-28 21:05:32 +0900

    이전 다음 글 css깨지는 거 수정

--- src/postage/pre_next_post.js
+++ src/postage/pre_next_post.js
@@ -34,10 +34,17 @@ class Pre_and_next_post extends React.Component {
       body: JSON.stringify(post),
     })
       .then((res) => res.json())
-
+      //1이 이전 0이 다음
+      
       .then((res) => {
-        this.setState({ pre_post: res[1], next_post: res[0] });
+        if(res[1] == undefined){
+          this.setState({pre_post: res[0],next_post: undefined});
+        }
+        else{
+          this.setState({ next_post: res[0] ,pre_post: res[1]});
+        }
       });
+      
   }
 
   render() {
@@ -45,36 +52,33 @@ class Pre_and_next_post extends React.Component {
 
     return (
       <div className="pre_and_next_post">
-        <Paper elevation = {0}>
+        <Paper className= "pre_and_next_post_paper"elevation = {0} square={false}>
         <TableContainer>
           <Table>
             <TableBody>
               {pre_post == undefined && (
-                <TableRow className="pre_row_n">
-                  <TableCell className="pre_n" width="150px" height="29.6px">
+                <TableRow className="NextOrPreNoneExistTR">
+                  <TableCell className="NextOrPreNoneExist" width="150px">
                     이전글이 없습니다
                   </TableCell>
                 </TableRow>
               )}
 
               {pre_post !== undefined && (
-                <TableRow>
+                <TableRow className="NextOrPreExistTR">
                   <TableCell
                     onClick={() => window.location.reload()}
-                    className="pre_y"
-                    width="80px"
+                    className="NextOrPreExist_nextorpre"
                   >
                     <Link
                       to={`/${this.props.board_name}/view/id=${pre_post.postage_key}&board=${this.props.board_key}`}
                     >
-                      이전 글<ArrowDropUpIcon />
+                      이전 글<ArrowDropUpIcon className="arrowButton"/>
                     </Link>
                   </TableCell>
                   <TableCell
                     onClick={() => window.location.reload()}
-                    className="pre_y"
-                    align="left"
-                    width="420px"
+                    className="NextOrPreExist_title"
                   >
                     <Link
                       to={`/${this.props.board_name}/view/id=${pre_post.postage_key}&board=${this.props.board_key}`}
@@ -83,48 +87,45 @@ class Pre_and_next_post extends React.Component {
                     </Link>
                     <font color="#006CB7"> [{pre_post.postage_comment}]</font>
                   </TableCell>
-                  <TableCell className="pre_y" align="center" width="100px">
-                    {pre_post.nickname}
+                  <TableCell className="NextOrPreExist_nickname">
+                    {pre_post.user_nickname}
                   </TableCell>
-                  <TableCell className="pre_y" align="center">
+                  <TableCell className="NextOrPreExist_like">
                     <FavoriteIcon color="secondary" />
                     {pre_post.postage_love}
                   </TableCell>
-                  <TableCell className="pre_y" align="center">
+                  <TableCell className="NextOrPreExist_date">
                     {pre_post.post_date}
                   </TableCell>
-                  <TableCell className="pre_y" align="center">
-                    <VisibilityIcon />
+                  <TableCell className="NextOrPreExist_views">
+                    <VisibilityIcon className="viewIcon"/>
                     {pre_post.postage_views}
                   </TableCell>
                 </TableRow>
               )}
 
               {next_post === undefined && (
-                <TableRow className="next_row_n">
-                  <TableCell className="next_n" width="150px" height="29.6px">
+                <TableRow className="NextOrPreNoneExistTR">
+                  <TableCell className="NextOrPreNoneExist" width="150px">
                     다음글이 없습니다
                   </TableCell>
                 </TableRow>
               )}
               {next_post !== undefined && (
-                <TableRow>
+                <TableRow className="NextOrPreExistTR">
                   <TableCell
                     onClick={() => window.location.reload()}
-                    className="next_y"
-                    width="80px"
+                    className="NextOrPreExist_nextorpre"
                   >
                     <Link
                       to={`/${this.props.board_name}/view/id=${next_post.postage_key}&board=${this.props.board_key}`}
                     >
-                      다음 글<ArrowDropDownIcon />
+                      다음 글<ArrowDropDownIcon className="arrowButton"/>
                     </Link>
                   </TableCell>
                   <TableCell
                     onClick={() => window.location.reload()}
-                    align="left"
-                    className="next_y"
-                    width="420px"
+                    className="NextOrPreExist_title"
                   >
                     <Link
                       to={`/${this.props.board_name}/view/id=${next_post.postage_key}&board=${this.props.board_key}`}
@@ -133,18 +134,18 @@ class Pre_and_next_post extends React.Component {
                     </Link>
                     <font color="#006CB7"> [{next_post.postage_comment}]</font>
                   </TableCell>
-                  <TableCell align="center" className="next_y" width="100px">
-                    {next_post.nickname}
+                  <TableCell className="NextOrPreExist_nickname">
+                    {next_post.user_nickname}
                   </TableCell>
-                  <TableCell align="center" className="next_y1">
+                  <TableCell className="NextOrPreExist_like">
                     <FavoriteIcon color="secondary" />
                     {next_post.postage_love}
                   </TableCell>
-                  <TableCell align="center" className="next_y">
+                  <TableCell className="NextOrPreExist_date">
                     {next_post.post_date}
                   </TableCell>
-                  <TableCell align="center" className="next_y2">
-                    <VisibilityIcon />
+                  <TableCell className="NextOrPreExist_views">
+                    <VisibilityIcon className="viewIcon"/>
                     {next_post.postage_views}
                   </TableCell>
                 </TableRow>
